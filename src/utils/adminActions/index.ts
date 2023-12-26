84459285e46b831ffcfa8a14cc75d4d513004111
PYAG1 <gyekyeyaw3@gmail.com>
2023-12-26 21:27:15 +0000

    Create

--- src/utils/adminActions/index.ts
+++ src/utils/adminActions/index.ts
@@ -15,10 +15,27 @@ interface SignUpValues {
   }
 
 
+  type EmployeeData = {
+    email: string;
+    firstname: string;
+    lastname: string;
+    gender: string;
+    contact: string;
+    role: string;
+    salary: string; // You might want to change this to a numeric type if salary is a number
+    departmentId: string;
+  };
+  
+
 export const signUpMutation = (data:SignUpValues) => {
     return axios.post(`${BASE_URL}/api/admin/register`, data)
 }
 export const adminSignInMutation= (data:SigninValues)=>{
     return axios.post(`${BASE_URL}/api/admin/login`, data)
 }
+
+export const CreateEmployeeMutation= (data:EmployeeData)=>{
+  return axios.post(`${BASE_URL}/api/admin/login`, data)
+}
+
 
